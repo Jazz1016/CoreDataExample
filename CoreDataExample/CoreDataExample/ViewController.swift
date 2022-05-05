db2add8649765beff8e83aee551673b9ad535419
James L <59751464+Jazz1016@users.noreply.github.com>
2022-05-04 19:09:37 -0600

    core data test project completed, gonna do one with a network call next

--- CoreDataExample/CoreDataExample/ViewController.swift
+++ CoreDataExample/CoreDataExample/ViewController.swift
@@ -20,27 +20,75 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         title                   = "CoreData To Do List"
         view.addSubview(tableView)
+        getAllItems()
         tableView.delegate      = self
         tableView.dataSource    = self
         tableView.frame = view.bounds
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+    }
+    
+    @objc func didTapAdd() {
+        let alert = UIAlertController(title: "New Item", message: "Enter New Item", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: "Submit", style: .cancel, handler: { [weak self] _ in
+            guard let field = alert.textFields?.first,
+                  let text  = field.text,
+                  !text.isEmpty,
+                  let self = self else { return }
+            self.createItem(name: text)
+        }))
+        
+        present(alert, animated: true)
     }
     
     private var models = [ToDoListItem]()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return models.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "hello world"
+        let model = models[indexPath.row]
+        cell.textLabel?.text = model.name
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = models[indexPath.row]
+        let sheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+//        alert.addTextField(configurationHandler: nil)
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { [weak self] _ in
+            let alert = UIAlertController(title: "Edit Item", message: "Enter New Item", preferredStyle: .alert)
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.name
+            alert.addAction(UIAlertAction(title: "Save", style: .cancel, handler: { _ in
+                guard let field = alert.textFields?.first,
+                      let newName = field.text,
+                      !newName.isEmpty,
+                      let self = self else { return }
+                self.updateItem(item: item, newName: newName)
+            }))
+            self?.present(alert, animated: true)
+        }))
+        
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { [weak self] _ in
+            self?.deleteItem(item: item)
+        }))
+        self.present(sheet, animated: true)
+    }
+    
     //Core Data
-    func getAllItem() {
+    func getAllItems() {
         do {
             models = try context.fetch(ToDoListItem.fetchRequest())
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         } catch {
          //error
         }
@@ -52,6 +100,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         newItem.createdAt   = Date()
         do {
             try context.save()
+            getAllItems()
         } catch {
             //error
         }
@@ -61,6 +110,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         context.delete(item)
         do {
             try context.save()
+            getAllItems()
         } catch {
             //error
         }
@@ -70,6 +120,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         item.name = newName
         do {
             try context.save()
+            getAllItems()
         } catch {
             //error
         }
